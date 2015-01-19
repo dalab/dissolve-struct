29df20018803caf4995ad25374e3a8071829e5b7
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2015-01-19 16:12:02 +0100

    Adding script to retrieve datasets

---
+++ data/retrieve_datasets.sh
@@ -0,0 +1,10 @@
+# Adult
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a1a
+
+# Forest Cover (Binary)
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/covtype.libsvm.binary.scale.bz2
+bzip2 -d covtype.libsvm.binary.scale.bz2 
+
+# RCV1
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/rcv1_train.binary.bz2
+bzip2 -d rcv1_train.binary.bz2
