f1fce416c4c436e729037b8099aabe14d034c0d7
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2015-03-12 18:26:49 +0100

    Brutus bootstrap also moves data to HDFS

--- helpers/brutus_setup.py
+++ helpers/brutus_setup.py
@@ -3,6 +3,9 @@
 __author__ = 'tribhu'
 
 import os
+import argparse
+
+from benchmark_utils import ssh
 
 from retrieve_datasets import retrieve, download_to_gen_dir
 from paths import PROJECT_DIR
@@ -13,6 +16,15 @@ SCOPT_JAR_PATH = 'https://dl.dropboxusercontent.com/u/12851272/scopt_2.10-3.3.0.
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Setup Brutus cluster for dissolve^struct')
+    parser.add_argument("username", help="Username (which has access to Hadoop Cluster)")
+    args = parser.parse_args()
+
+    username = args.username
+
+    def ssh_brutus(command):
+        ssh('hadoop', username, command)
+
     # === Obtain data ===
     retrieve(download_all=True)
 
@@ -21,10 +33,15 @@ def main():
     if not os.path.exists(jars_dir):
         os.makedirs(jars_dir)
 
-    print "Downloading executables to: ", jars_dir
+    print "=== Downloading executables to: ", jars_dir, "==="
     for jar_url in [LIB_JAR_URL, EXAMPLES_JAR_PATH, SCOPT_JAR_PATH]:
         print "== Retrieving ", jar_url.split('/')[-1], '=='
         download_to_gen_dir(jar_url, jars_dir)
+
+    # === Move data to HDFS ===
+    print "=== Moving data to HDFS ==="
+    put_data_cmd = "hadoop fs -put dissolve-struct/data /user/%s/data" % username
+    ssh_brutus(put_data_cmd)
 
 
 if __name__ == '__main__':
