e151d61753193a268115c076092d9e4bb9892633
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2015-12-11 13:48:29 +0100

    Fixed gapCheck and timeBudget in LocalBCFW

--- dissolve-struct-lib/src/main/scala/ch/ethz/dalab/dissolve/optimization/LocalBCFW.scala
+++ dissolve-struct-lib/src/main/scala/ch/ethz/dalab/dissolve/optimization/LocalBCFW.scala
@@ -33,15 +33,15 @@ class LocalBCFW[X, Y](
     numPasses: Int = 200,
     doLineSearch: Boolean = true,
     doWeightedAveraging: Boolean = true,
-    timeBudget: Int = Integer.MAX_VALUE, // TODO
+    timeBudget: Int = Integer.MAX_VALUE,
     debug: Boolean = false,
     debugMultiplier: Int = 100,
     debugOutPath: String = "debug-%d.csv".format(System.currentTimeMillis()),
     randSeed: Long = 1,
     randomSampling: Boolean = false,
     lambda: Double = 0.01,
-    gapThreshold: Double = 0.1, // TODO
-    gapCheck: Int = 0, // TODO
+    gapThreshold: Double = 0.1,
+    gapCheck: Int = 0,
     enableOracleCache: Boolean = false,
     oracleCacheSize: Int = 10) extends LocalSolver[X, Y] {
 
@@ -105,9 +105,6 @@ class LocalBCFW[X, Y](
   def train(data: Seq[LabeledObject[X, Y]],
             testData: Option[Seq[LabeledObject[X, Y]]])(implicit m: ClassTag[Y]): StructSVMModel[X, Y] = {
 
-    val verboseDebug: Boolean = false
-    val debugWeights: Boolean = false
-
     util.Random.setSeed(randSeed)
 
     // Number of dimensions of \phi(x, y)
@@ -140,7 +137,7 @@ class LocalBCFW[X, Y](
       else
         debugSb ++= "round,time,iter,primal,dual,gap,train_error\n"
     }
-    val startTime = System.currentTimeMillis()
+    val startTimeMillis = System.currentTimeMillis()
 
     if (debug) {
       println("Beginning training of %d data points in %d passes with lambda=%f".format(n, numPasses, lambda))
@@ -149,163 +146,177 @@ class LocalBCFW[X, Y](
     // Initialize the cache: Index -> List of precomputed ystar_i's
     var oracleCache = collection.mutable.Map[Int, MutableList[Y]]()
 
-    for (passNum <- 0 until numPasses) {
+    // Serves as a flag to determine when to end solver iterations
+    // This could be flipped either due to: gapCheck or timeBudget
+    var terminateIter: Boolean = false
 
-      if (verboseDebug) {
-        println("wMat before pass: " + model.getWeights()(0 to 10).toDenseVector)
-        println("ellmat before pass: " + ellMat(0 to 10))
-        println("Ell before pass = " + ell)
-      }
+    (0 until numPasses).toStream
+      .takeWhile(_ => !terminateIter)
+      .foreach {
 
-      val idxSeq = (0 until n).toList
-      val perm = util.Random.shuffle(idxSeq)
+        passNum =>
+          val idxSeq = (0 until n).toList
+          val perm = util.Random.shuffle(idxSeq)
 
-      for (dummy <- 0 until n) {
-        // 1) Pick example
-        val i: Int = if (randomSampling) util.Random.nextInt(n) else perm(dummy)
-        val pattern: X = data(i).pattern
-        val label: Y = data(i).label
+          (0 until n).toStream
+            .takeWhile(_ => !terminateIter)
+            .foreach {
 
-        // 2) Solve loss-augmented inference for point i
-        // 2.a) If cache is enabled, check if any of the previous ystar_i's for this i can be used
-        val bestCachedCandidateForI: Option[Y] =
-          if (enableOracleCache && oracleCache.contains(i)) {
-            val candidates: Seq[(Double, Int)] =
-              oracleCache(i)
-                .map(y_i => (((phi(pattern, label) - phi(pattern, y_i)) :* (1 / (n * lambda))),
-                  (1.0 / n) * lossFn(label, y_i))) // Map each cached y_i to their respective (w_s, ell_s)
-                .map {
-                  case (w_s, ell_s) => (model.getWeights().t * (wMat(::, i) - w_s) - ((ellMat(i) - ell_s) * (1 / lambda))) /
-                    ((wMat(::, i) - w_s).t * (wMat(::, i) - w_s) + eps) // Map each (w_s, ell_s) to their respective step-size values	
+              dummy =>
+                // 1) Pick example
+                val i: Int = if (randomSampling) util.Random.nextInt(n) else perm(dummy)
+                val pattern: X = data(i).pattern
+                val label: Y = data(i).label
+
+                // 2) Solve loss-augmented inference for point i
+                // 2.a) If cache is enabled, check if any of the previous ystar_i's for this i can be used
+                val bestCachedCandidateForI: Option[Y] =
+                  if (enableOracleCache && oracleCache.contains(i)) {
+                    val candidates: Seq[(Double, Int)] =
+                      oracleCache(i)
+                        .map(y_i => (((phi(pattern, label) - phi(pattern, y_i)) :* (1 / (n * lambda))),
+                          (1.0 / n) * lossFn(label, y_i))) // Map each cached y_i to their respective (w_s, ell_s)
+                        .map {
+                          case (w_s, ell_s) => (model.getWeights().t * (wMat(::, i) - w_s) - ((ellMat(i) - ell_s) * (1 / lambda))) /
+                            ((wMat(::, i) - w_s).t * (wMat(::, i) - w_s) + eps) // Map each (w_s, ell_s) to their respective step-size values	
+                        }
+                        .zipWithIndex // We'll need the index later to retrieve the respective approx. ystar_i
+                        .filter { case (gamma, idx) => gamma > 0.0 }
+                        .map { case (gamma, idx) => (min(1.0, gamma), idx) } // Clip to [0,1] interval
+                        .sortBy { case (gamma, idx) => gamma }
+
+                    val naive_gamma: Double = (2.0 * n) / (k + 2.0 * n)
+
+                    // If there is a good contender among the cached datapoints, return it
+                    if (candidates.size >= 1)
+                      Some(oracleCache(i)(candidates.head._2))
+                    else None
+                  } else
+                    None
+
+                // 2.b) In case cache is disabled or a good contender from cache hasn't been found, call max Oracle
+                val ystar_i: Y =
+                  if (bestCachedCandidateForI.isEmpty) {
+                    val ystar = maxOracle(model, pattern, label)
+
+                    if (enableOracleCache)
+                      // Add this newly computed ystar to the cache of this i
+                      oracleCache.update(i, if (oracleCacheSize > 0)
+                        { oracleCache.getOrElse(i, MutableList[Y]()) :+ ystar }.takeRight(oracleCacheSize)
+                      else { oracleCache.getOrElse(i, MutableList[Y]()) :+ ystar })
+                    // kick out oldest if max size reached
+                    ystar
+                  } else {
+                    bestCachedCandidateForI.get
+                  }
+
+                // 3) Define the update quantities
+                val psi_i: Vector[Double] = phi(pattern, label) - phi(pattern, ystar_i)
+                val w_s: Vector[Double] = psi_i :* (1 / (n * lambda))
+                val loss_i: Double = lossFn(label, ystar_i)
+                val ell_s: Double = (1.0 / n) * loss_i
+
+                // 4) Get step-size gamma
+                val gamma: Double =
+                  if (doLineSearch) {
+                    val gamma_opt = (model.getWeights().t * (wMat(::, i) - w_s) - ((ellMat(i) - ell_s) * (1 / lambda))) /
+                      ((wMat(::, i) - w_s).t * (wMat(::, i) - w_s) + eps)
+                    max(0.0, min(1.0, gamma_opt))
+                  } else {
+                    (2.0 * n) / (k + 2.0 * n)
+                  }
+
+                // 5, 6, 7, 8) Update the weights of the model
+                val tempWeights1: Vector[Double] = model.getWeights() - wMat(::, i)
+                model.setWeights(tempWeights1)
+                wMat(::, i) := (wMat(::, i) * (1.0 - gamma)) + (w_s * gamma)
+                val tempWeights2: Vector[Double] = model.getWeights() + wMat(::, i)
+                model.setWeights(tempWeights2)
+
+                ell = ell - ellMat(i)
+                ellMat(i) = (ellMat(i) * (1.0 - gamma)) + (ell_s * gamma)
+                ell = ell + ellMat(i)
+
+                // 9) Optionally update the weighted average
+                if (doWeightedAveraging) {
+                  val rho: Double = 2.0 / (k + 2.0)
+                  wAvg = (wAvg * (1.0 - rho)) + (model.getWeights * rho)
+                  lAvg = (lAvg * (1.0 - rho)) + (ell * rho)
                 }
-                .zipWithIndex // We'll need the index later to retrieve the respective approx. ystar_i
-                .filter { case (gamma, idx) => gamma > 0.0 }
-                .map { case (gamma, idx) => (min(1.0, gamma), idx) } // Clip to [0,1] interval
-                .sortBy { case (gamma, idx) => gamma }
 
-            // TODO Use this naive_gamma to further narrow down on cached contenders
-            // TODO Maintain fixed size of the list of cached vectors
-            val naive_gamma: Double = (2.0 * n) / (k + 2.0 * n)
+                /**
+                 * DEBUG/TEST code
+                 */
+                k = k + 1
 
-            // If there is a good contender among the cached datapoints, return it
-            if (candidates.size >= 1)
-              Some(oracleCache(i)(candidates.head._2))
-            else None
-          } else
-            None
+                if (debug && k >= debugIter) {
+                  if (doWeightedAveraging) {
+                    debugModel.setWeights(wAvg)
+                    debugModel.setEll(lAvg)
+                  } else {
+                    debugModel.setWeights(model.getWeights)
+                    debugModel.setEll(ell)
+                  }
 
-        // 2.b) In case cache is disabled or a good contender from cache hasn't been found, call max Oracle
-        val ystar_i: Y =
-          if (bestCachedCandidateForI.isEmpty) {
-            val ystar = maxOracle(model, pattern, label)
+                  val f = -SolverUtils.objectiveFunction(debugModel.getWeights(), debugModel.getEll(), lambda)
+                  val gapTup = SolverUtils.dualityGap(data, phi, lossFn, maxOracle, debugModel, lambda)
+                  val gap = gapTup._1
+                  val primal = f + gap
 
-            if (enableOracleCache)
-              // Add this newly computed ystar to the cache of this i
-              oracleCache.update(i, if (oracleCacheSize > 0)
-                { oracleCache.getOrElse(i, MutableList[Y]()) :+ ystar }.takeRight(oracleCacheSize)
-              else { oracleCache.getOrElse(i, MutableList[Y]()) :+ ystar })
-            // kick out oldest if max size reached
-            ystar
-          } else {
-            bestCachedCandidateForI.get
+                  val trainError = SolverUtils.averageLoss(data, dissolveFunctions, debugModel)
+                  val testError =
+                    if (testData.isDefined)
+                      SolverUtils.averageLoss(testData.get, dissolveFunctions, debugModel)
+                    else
+                      (0.00, 0.00)
+
+                  val curTime = (System.currentTimeMillis() - startTimeMillis) / 1000.0
+
+                  println("Pass %d Iteration %d, SVM primal = %f, SVM dual = %f, Duality gap = %f, Train error = %f, Test error = %f"
+                    .format(passNum + 1, k, primal, f, gap, trainError._1, testError._1))
+
+                  debugSb ++= "%d,%f,%d,%s,%s,%s,%f,%f\n"
+                    .format(passNum + 1, curTime, k,
+                      primal, f, gap,
+                      trainError._1, testError._1)
+
+                  debugIter = min(debugIter + n, ceil(debugIter * (1 + debugMultiplierCor / 100)))
+                }
+
+                // Check if time budget is exceeded
+                val elapsedTimeMillis = System.currentTimeMillis() - startTimeMillis
+                val elapsedTimeMins = elapsedTimeMillis / (1000.0 * 60.0)
+                if (elapsedTimeMins >= timeBudget) {
+                  println("Time budget exceeded. Stopping.")
+                  println("Elapsed Time : %s mins. TimeBudget : %s mins".format(elapsedTimeMins, timeBudget))
+                  terminateIter = true
+                }
+
+            }
+
+          // Check if gap criterion is met
+          if (gapCheck > 0 && (passNum + 1) % gapCheck == 0) {
+
+            if (doWeightedAveraging) {
+              debugModel.setWeights(wAvg)
+              debugModel.setEll(lAvg)
+            } else {
+              debugModel.setWeights(model.getWeights)
+              debugModel.setEll(ell)
+            }
+
+            val gapTup = SolverUtils.dualityGap(data, phi, lossFn, maxOracle, debugModel, lambda)
+            val gap = gapTup._1
+
+            if (gap <= gapThreshold) {
+              println("Duality gap requirement satisfied. Stopping.")
+              println("Current gap: %s. Gap threshold = %s".format(gap, gapThreshold))
+              terminateIter = true
+            }
+
           }
-
-        if (i < 5 && verboseDebug) {
-          println("y_star(%d) = %s".format(i, ystar_i))
-        }
-
-        // 3) Define the update quantities
-        val psi_i: Vector[Double] = phi(pattern, label) - phi(pattern, ystar_i)
-        val w_s: Vector[Double] = psi_i :* (1 / (n * lambda))
-        val loss_i: Double = lossFn(label, ystar_i)
-        val ell_s: Double = (1.0 / n) * loss_i
-
-        // 4) Get step-size gamma
-        val gamma: Double =
-          if (doLineSearch) {
-            val gamma_opt = (model.getWeights().t * (wMat(::, i) - w_s) - ((ellMat(i) - ell_s) * (1 / lambda))) /
-              ((wMat(::, i) - w_s).t * (wMat(::, i) - w_s) + eps)
-            max(0.0, min(1.0, gamma_opt))
-          } else {
-            (2.0 * n) / (k + 2.0 * n)
-          }
-
-        // 5, 6, 7, 8) Update the weights of the model
-        val tempWeights1: Vector[Double] = model.getWeights() - wMat(::, i)
-        model.setWeights(tempWeights1)
-        wMat(::, i) := (wMat(::, i) * (1.0 - gamma)) + (w_s * gamma)
-        val tempWeights2: Vector[Double] = model.getWeights() + wMat(::, i)
-        model.setWeights(tempWeights2)
-
-        ell = ell - ellMat(i)
-        ellMat(i) = (ellMat(i) * (1.0 - gamma)) + (ell_s * gamma)
-        ell = ell + ellMat(i)
-
-        // 9) Optionally update the weighted average
-        if (doWeightedAveraging) {
-          val rho: Double = 2.0 / (k + 2.0)
-          wAvg = (wAvg * (1.0 - rho)) + (model.getWeights * rho)
-          lAvg = (lAvg * (1.0 - rho)) + (ell * rho)
-        }
-
-        /**
-         * DEBUG/TEST code
-         */
-        // If this is the last pass and debugWeights flag is true, dump weight vector to CSV
-        if (debugWeights && dummy == (n - 1))
-          csvwrite(new File("data/debug/debugWeights/scala-w-%d.csv".format(passNum + 1)),
-            { if (doWeightedAveraging) wAvg else model.getWeights }.toDenseVector.toDenseMatrix)
-
-        k = k + 1
-
-        if (debug && k >= debugIter) {
-          if (doWeightedAveraging) {
-            debugModel.setWeights(wAvg)
-            debugModel.setEll(lAvg)
-          } else {
-            debugModel.setWeights(model.getWeights)
-            debugModel.setEll(ell)
-          }
-
-          val f = -SolverUtils.objectiveFunction(debugModel.getWeights(), debugModel.getEll(), lambda)
-          val gapTup = SolverUtils.dualityGap(data, phi, lossFn, maxOracle, debugModel, lambda)
-          val gap = gapTup._1
-          val primal = f + gap
-          val trainError = SolverUtils.averageLoss(data, dissolveFunctions, debugModel)
-
-          if (verboseDebug) {
-            println("wMat after pass: " + model.getWeights()(0 to 10).toDenseVector)
-            println("ellmat after pass: " + ellMat(0 to 10))
-            println("Ell after pass = " + ell)
-
-            debugSb ++= "# sum(w): %f, ell: %f\n".format(debugModel.getWeights().sum, debugModel.getEll())
-          }
-
-          val curTime = (System.currentTimeMillis() - startTime) / 1000.0
-
-          if (testData != null) {
-            val testError =
-              if (testData.isDefined)
-                SolverUtils.averageLoss(testData.get, dissolveFunctions, debugModel)
-              else
-                (0.00, 0.00)
-            println("Pass %d Iteration %d, SVM primal = %f, SVM dual = %f, Duality gap = %f, Train error = %f, Test error = %f"
-              .format(passNum + 1, k, primal, f, gap, trainError._1, testError._1))
-            if (debug)
-              debugSb ++= "%d,%f,%d,%s,%s,%s,%f,%f\n".format(passNum + 1, curTime, k, primal.toString(), f.toString(), gap.toString(), trainError._1, testError._1)
-          } else {
-            println("Pass %d Iteration %d, SVM primal = %f, SVM dual = %f, Duality gap = %f, Train error = %f"
-              .format(passNum + 1, k, primal, f, gap, trainError._1))
-            if (debug)
-              debugSb ++= "%d,%f,%d,%s,%s,%s,%f\n".format(passNum + 1, curTime, k, primal.toString(), f.toString(), gap.toString(), trainError._1)
-          }
-
-          debugIter = min(debugIter + n, ceil(debugIter * (1 + debugMultiplierCor / 100)))
-        }
 
       }
-
-    }
 
     if (doWeightedAveraging) {
       model.setWeights(wAvg)
